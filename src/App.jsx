1c22db67e48f9b26aa4f2111fedcc7dbcf6c07c4
Amit Rawat <theamitrwt@gmail.com>
2026-01-23 00:21:24 +0530

    Starting New Production ready project 1. Add conf and .env 2. add authentication file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+      <h1>new project of blog</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl: String(VITE_APPWRITE_URL),
+    appwriteProjectId: String(VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(VITE_APPWRITE_DATABASE_ID),
+    appwriteUrlCollectionId: String(VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
